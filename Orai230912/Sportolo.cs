f51c1bed17ec72b018f2928d369367f2ccecad92
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-12 12:10:32 +0200

    Update Sportolo.cs

--- Orai230912/Sportolo.cs
+++ Orai230912/Sportolo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +11,13 @@ namespace Orai230912
         public string Nev { get; set; }
         public bool Kategoria { get; set; }
         public string Egyesulet { get; set; }
+        public int[] Pontszamok { get; set; }
+
+        public Sportolo(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Kategoria = v[1] == "Felnott ferfi";
+        }
     }
 }
