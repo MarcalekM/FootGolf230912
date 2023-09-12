dafd97ad3c5336255a697debdd9acba95472aea3
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-12 11:58:04 +0200

    Add files via upload

---
+++ Orai230912/Sportolo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orai230912
+{
+    class Sportolo
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+    }
+}
