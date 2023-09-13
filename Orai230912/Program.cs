738045c1d6be838de8f80e07828e9474dcf85158
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-13 13:58:55 +0200

    Feladat 3 and 4

--- Orai230912/Program.cs
+++ Orai230912/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace Orai230912
 {
@@ -6,7 +11,18 @@ namespace Orai230912
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sportolok = new List<Sportolo>();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\fob2016.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream) sportolok.Add(new Sportolo(sr.ReadLine()));
+            Console.WriteLine("3. feladat");
+            Console.WriteLine($"Versenyzők száma: {sportolok.Count()}");
+
+            Console.WriteLine("4. feladat");
+            int nsz = sportolok.Count(v => !v.Kategoria);
+            float na = nsz / (float)sportolok.Count() * 100;
+            Console.WriteLine($"Nói versenyzők aránya: {na:0.00}%");
         }
     }
 }
