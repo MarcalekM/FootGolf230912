106db758277bf8ffe1f1b386e2ff58c8a90f5702
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-14 10:05:04 +0200

    6. Feladat

--- Orai230912/Program.cs
+++ Orai230912/Program.cs
@@ -16,17 +16,19 @@ namespace Orai230912
                 path: @"..\..\..\src\fob2016.txt",
                 Encoding.UTF8);
             while (!sr.EndOfStream) sportolok.Add(new Sportolo(sr.ReadLine()));
-            Console.WriteLine("3. feladat");
-            Console.WriteLine($"Versenyzők száma: {sportolok.Count()}");
+            Console.WriteLine($"3. feladat: Versenyzők száma: {sportolok.Count()}");
 
-            Console.WriteLine("4. feladat");
             int nsz = sportolok.Count(v => !v.Kategoria);
             float na = nsz / (float)sportolok.Count() * 100;
-            Console.WriteLine($"Nói versenyzők aránya: {na:0.00}%");
+            Console.WriteLine($"4. feladat: Női versenyzők aránya: {na:0.00}%");
 
-            var nb = sportolok
+            var noiBajnok = sportolok
                 .Where(v => !v.Kategoria)
-                .OrderBy(v.)
+                .OrderBy(v => v.Osszpontszam)
+                .Last();
+            Console.WriteLine($"\tNév: {noiBajnok.Nev}");
+            Console.WriteLine($"\tEgyesület: {noiBajnok.Egyesulet}");
+            Console.WriteLine($"\tÖsszpontszám: {noiBajnok.Osszpontszam}");
         }
     }
 }
